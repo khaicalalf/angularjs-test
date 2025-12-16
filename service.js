f28e190e7c43210b30648d41d539a81a91ceac99
Muhammad Ghufron Khaical Alfaris <alfaris.mgk@gmail.com>
2025-12-16 11:11:05 +0700

    feat: add API key to all EmployeeService HTTP requests

--- service.js
+++ service.js
@@ -1,11 +1,22 @@
 app.service("EmployeeService", function ($http) {
   const BASE_URL = "http://localhost:3000/api/employees";
+  const API_KEY = "secret123";
 
-  this.getAll = () => $http.get(BASE_URL);
-  this.getById = (id) => $http.get(`${BASE_URL}/${id}`);
-  this.create = (data) => $http.post(BASE_URL, data);
-  this.update = (id, data) => $http.put(`${BASE_URL}/${id}`, data);
-  this.delete = (id) => $http.delete(`${BASE_URL}/${id}`);
+  const config = {
+    headers: {
+      "x-api-key": API_KEY,
+    },
+  };
+
+  this.getAll = () => $http.get(BASE_URL, config);
+
+  this.getById = (id) => $http.get(`${BASE_URL}/${id}`, config);
+
+  this.create = (data) => $http.post(BASE_URL, data, config);
+
+  this.update = (id, data) => $http.put(`${BASE_URL}/${id}`, data, config);
+
+  this.delete = (id) => $http.delete(`${BASE_URL}/${id}`, config);
 });
 
 app.service("DepartmentService", function ($http) {
